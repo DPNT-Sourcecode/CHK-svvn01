bc021bd5d3becf4035454fe222cebe17439bc6bf
sbx_user1051 <sbx_user1051@169.254.105.73>
2019-04-12 05:08:17 +0000

    Fri Apr 12 05:08:17 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,11 +1,49 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
-  STOCK_PRICES = { A: 50, B: 30, C: 20, D: 15, E: 40, F: 10 }
-  SPECIALS_QUANTS = { A: 5, B: 2, F: 3 }
-  SPECIALS_PRICES = { A: 200, B: 45, F: 20 }
-  SPECIALS_QUANTS_2 = { A: 3 }
-  SPECIALS_PRICES_2 = { A: 130 }
+  STOCK_PRICES = {
+    A: 50,
+    B: 30,
+    C: 20,
+    D: 15,
+    E: 40,
+    F: 10,
+    G: 20,
+    H: 10,
+    I: 35,
+    J: 60,
+    K: 80,
+    L: 90,
+    M: 15,
+    N: 40,
+    O: 10,
+    P: 50,
+    Q: 30,
+    R: 50,
+    S: 30,
+    T: 20,
+    U: 40,
+    V: 50,
+    W: 20,
+    X: 90,
+    Y: 10,
+    Z: 50 }
+
+  SPECIALS_QUANTS = {
+    A: 5,
+    B: 2,
+    F: 3 }
+
+  SPECIALS_PRICES = {
+    A: 200,
+    B: 45,
+    F: 20 }
+
+  SPECIALS_QUANTS_2 = {
+    A: 3 }
+    
+  SPECIALS_PRICES_2 = {
+    A: 130 }
 
   def checkout(skus)
     return -1 if check_skus(skus) == false
@@ -69,3 +107,4 @@ class Checkout
     order_summary[item] = remainder unless remainder.nil?
   end
 end
+
