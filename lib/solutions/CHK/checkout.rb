eaf0936969030208703cf6c99e30793531002544
sbx_user1051 <sbx_user1051@169.254.95.61>
2019-04-14 11:55:24 +0000

    Sun Apr 14 11:55:24 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -145,7 +145,8 @@ p item
 p    groups_remainder = group_items % 3
 p    item_quantity = order[item]
 p    order[item] -= groups_remainder if groups_remainder <= order[item]
-p    order[item] = 0 if groups_remainder > order[item]
+p    order[item] = 0 if groups_remainder > order[item] || groups_remainder == 0
 p    groups_remainder -= item_quantity - order[item]
   end
 end
+
