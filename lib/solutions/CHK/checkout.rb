d574cee25c1ba68273e39a452ee5887086b440c0
sbx_user1051 <sbx_user1051@169.254.95.61>
2019-04-14 12:07:24 +0000

    Sun Apr 14 12:07:24 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -134,21 +134,21 @@ class Checkout
     groups_quantity = group_items / 3
 p    @running_total += groups_quantity * 45
     GROUP.each do |item|
+p item
       remove_group_items(order, group_items, item) if order.key?(item)
     end
+p order
   end
 
   def remove_group_items(order, group_items, item)
-p order
-p group_items
-p item
-p    groups_remainder = group_items % 3
-p    item_quantity = order[item]
-p    order[item] -= groups_remainder if groups_remainder <= order[item]
-p    order[item] = 0 if groups_remainder > order[item] || groups_remainder == 0
-p    groups_remainder -= item_quantity - order[item] if groups_remainder >= 0
+    groups_remainder = group_items % 3
+    item_quantity = order[item]
+    order[item] -= groups_remainder if groups_remainder <= order[item]
+    order[item] = 0 if groups_remainder > order[item] || groups_remainder == 0
+    groups_remainder -= item_quantity - order[item] if groups_remainder >= 0
   end
 end
+
 
 
 
