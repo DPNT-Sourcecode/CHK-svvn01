519fb6be52c56b7c94f45e4301e82000d0d19ffd
sbx_user1051 <sbx_user1051@169.254.95.61>
2019-04-14 12:22:24 +0000

    Sun Apr 14 12:22:24 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -145,8 +145,9 @@ p order
   def remove_group_items(order, group_items, item)
     groups_remainder = group_items % 3
     item_quantity = order[item]
-    order[item] -= groups_remainder if groups_remainder <= order[item]
-    order[item] = 0 if groups_remainder > order[item] || groups_remainder == 0
-    groups_remainder -= item_quantity - order[item] if groups_remainder >= 0
+    order[item] = groups_remainder if groups_remainder <= order[item]
+    order[item] = 0 if groups_remainder > order[item]
+    groups_remainder -= item_quantity - order[item]
   end
 end
+
